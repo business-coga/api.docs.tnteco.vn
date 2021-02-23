3e1d99bdece5551054b213a443c9932b0d9302fd
byndiorpro182 <byndiorpro182@gmail.com>
2021-02-23 17:01:31 +0700

    fix

--- website/index.js
+++ website/index.js
@@ -2,18 +2,15 @@ require('dotenv').config()
 const
   express = require('express'),
   serveStatic = require('serve-static'),
-  history = require('connect-history-api-fallback'),
+//   history = require('connect-history-api-fallback'),
   port = process.env.PORT
 
-const fs = require('fs');
-
 const app = express()
-app.use(history())
-app.use(serveStatic(__dirname + '/build/test-site'))
+// app.use(history())
 app.get('/ping', function (req, res) {
-  res.status(200).send('pong').end()
-})
-
+    res.status(200).send('pong').end()
+  })
+app.use(serveStatic(__dirname + '/build/test-site'))
 app.listen(port, () => {
 	console.log(`Server running on http://api.docs.tnteco.vn:${port}`);
 });
